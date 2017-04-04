46c4de2fcb82596fd4dbb4f95546f9f80b119f67
diegocairone <diegocairone@gmail.com>
2017-04-04 13:05:10 -0300

    Soporte para Logical Operators (5.1.1.1) en Provincias

---
+++ odataexample/src/main/java/com/cairone/odataexample/converters/QueryDslOpsConverter.java
@@ -0,0 +1,32 @@
+package com.cairone.odataexample.converters;
+
+import com.cairone.odataexample.interfaces.Converter;
+import com.mysema.query.types.Operator;
+import com.mysema.query.types.Ops;
+
+public class QueryDslOpsConverter implements Converter<String, Operator<Boolean>> {
+
+	@Override
+	public Operator<Boolean> convertToAnotherAttribute(String anotherAttribute) {
+
+		switch(anotherAttribute) {
+		case "=":
+			return Ops.EQ;
+		case "<>":
+			return Ops.NE;
+		case "<":
+			return Ops.LT;
+		case "<=":
+			return Ops.LOE;
+		case ">":
+			return Ops.GT;
+		case ">=":
+			return Ops.GOE;
+		case "AND":
+			return Ops.AND;
+		case "OR":
+			return Ops.OR;
+		}
+		return null;
+	}
+}
--- odataexample/src/main/java/com/cairone/odataexample/strategyBuilders/ProvinciasStrategyBuilder.java
+++ odataexample/src/main/java/com/cairone/odataexample/strategyBuilders/ProvinciasStrategyBuilder.java
@@ -4,19 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 
 import scala.collection.Iterator;
 
+import com.cairone.odataexample.converters.QueryDslOpsConverter;
 import com.cairone.odataexample.entities.QProvinciaEntity;
+import com.mysema.query.support.Expressions;
+import com.mysema.query.types.Constant;
+import com.mysema.query.types.Path;
 import com.mysema.query.types.expr.BooleanExpression;
+import com.mysema.query.types.expr.BooleanOperation;
 import com.sdl.odata.api.ODataException;
 import com.sdl.odata.api.ODataSystemException;
 import com.sdl.odata.api.parser.CountOption;
 import com.sdl.odata.api.parser.ODataUriUtil;
 import com.sdl.odata.api.parser.QueryOption;
 import com.sdl.odata.api.processor.query.ComparisonCriteria;
+import com.sdl.odata.api.processor.query.CompositeCriteria;
 import com.sdl.odata.api.processor.query.ContainsMethodCriteria;
 import com.sdl.odata.api.processor.query.CountOperation;
 import com.sdl.odata.api.processor.query.Criteria;
@@ -36,6 +44,8 @@ import com.sdl.odata.api.service.ODataRequestContext;
 
 public class ProvinciasStrategyBuilder {
 
+	private static Logger logger = LoggerFactory.getLogger(ProvinciasStrategyBuilder.class);
+	
 	private QProvinciaEntity qProvincia = null;
 	private BooleanExpression expression = null;
 	
@@ -45,10 +55,12 @@ public class ProvinciasStrategyBuilder {
     private int skip = 0;
     private boolean count;
 	private boolean includeCount;
+	private QueryDslOpsConverter queryDslOpsConverter;
 
 	public BooleanExpression buildCriteria(QueryOperation queryOperation, ODataRequestContext requestContext) throws ODataException {
 		
 		qProvincia = QProvinciaEntity.provinciaEntity;
+		queryDslOpsConverter = new QueryDslOpsConverter();
 
 		buildFromOperation(queryOperation);
         buildFromOptions(ODataUriUtil.getQueryOptions(requestContext.getUri()));
@@ -118,25 +130,59 @@ public class ProvinciasStrategyBuilder {
     }
 
     private void buildFromFilter(CriteriaFilterOperation criteriaFilterOperation) throws ODataException {
-    	
-    	Criteria criteria = criteriaFilterOperation.getCriteria();
-        
-    	if(criteria instanceof ComparisonCriteria) {
-    		
-            ComparisonCriteria comparisonCriteria = (ComparisonCriteria) criteria;
-            execComparisonCriteria(comparisonCriteria);
-        }
 
-    	if(criteria instanceof ContainsMethodCriteria) {
-    		
-    		ContainsMethodCriteria containsMethodCriteria = (ContainsMethodCriteria) criteria;
-    		execContainsMethodCriteria(containsMethodCriteria);
+    	Criteria criteria = criteriaFilterOperation.getCriteria();
+    	logger.info("Criteria type: {}", criteria.toString());
+        
+    	boolean isComposite = criteria instanceof CompositeCriteria;
+    	BooleanExpression exp = isComposite ? execCompositeCriteria((CompositeCriteria) criteria) : execSimpleCriteria(criteria);
+    	
+    	if(exp != null) {
+    		this.expression = this.expression == null ? exp : this.expression.and(exp);
     	}
     	
     	buildFromOperation(criteriaFilterOperation.getSource());
     }
+    
+    private BooleanExpression execCompositeCriteria(Criteria criteria) {
+    	
+    	if(criteria instanceof CompositeCriteria) {
+	    	
+    		CompositeCriteria compositeCriteria = (CompositeCriteria) criteria;
+    		
+	    	Criteria criteriaLeft = compositeCriteria.getLeft();
+	    	Criteria criteriaRight = compositeCriteria.getRight();
+	    	String operator = compositeCriteria.operator().toString();
+	    	
+	    	boolean isCompositeLeft = criteriaLeft instanceof CompositeCriteria;
+	    	boolean isCompositeRight = criteriaRight instanceof CompositeCriteria;
+	    	
+	    	BooleanExpression expLeft = isCompositeLeft ? execCompositeCriteria(criteriaLeft) : execSimpleCriteria(criteriaLeft);
+	    	BooleanExpression expRight = isCompositeRight ? execCompositeCriteria(criteriaRight) : execSimpleCriteria(criteriaRight);
+	    	BooleanExpression exp = BooleanOperation.create(queryDslOpsConverter.convertToAnotherAttribute(operator), expLeft, expRight);
+	    	
+	    	return exp;
+    	}
+    	
+    	return null;
+    }
+    
+    private BooleanExpression execSimpleCriteria(Criteria criteria) {
 
-    private void execContainsMethodCriteria(ContainsMethodCriteria containsMethodCriteria) {
+    	if(criteria instanceof ComparisonCriteria) {
+    		ComparisonCriteria comparisonCriteria = (ComparisonCriteria) criteria;
+            return execComparisonCriteria(comparisonCriteria);
+        }
+
+    	if(criteria instanceof ContainsMethodCriteria) {
+    		ContainsMethodCriteria containsMethodCriteria = (ContainsMethodCriteria) criteria;
+    		return execContainsMethodCriteria(containsMethodCriteria);
+    	}
+    	
+    	return null;
+    }
+
+    private BooleanExpression execContainsMethodCriteria(ContainsMethodCriteria containsMethodCriteria) {
 
 		PropertyCriteriaValue propertyCriteriaValue = (PropertyCriteriaValue) containsMethodCriteria.getProperty();
         LiteralCriteriaValue literalCriteriaValue = (LiteralCriteriaValue) containsMethodCriteria.getStringLiteral();
@@ -149,63 +195,104 @@ public class ProvinciasStrategyBuilder {
         case "NOMBRE":
         {
         	String nombre = value.toString();
-        	BooleanExpression exp = qProvincia.nombre.contains(nombre);
-            this.expression = this.expression == null ? exp : this.expression.and(exp);
-        	break;
+        	return qProvincia.nombre.contains(nombre);
+        }
+        case "PAIS.NOMBRE":
+        {
+        	String nombre = value.toString();
+        	return qProvincia.pais.nombre.contains(nombre);
         }
         }
+        
+        return null;
+    }
+    
+    private BooleanExpression execComparisonCriteria(ComparisonCriteria comparisonCriteria) {
+
+        PropertyCriteriaValue propertyCriteriaValue = getPropertyCriteriaValue(comparisonCriteria);
+        LiteralCriteriaValue literalCriteriaValue = getLiteralCriteriaValue(comparisonCriteria);
+        
+        String field = propertyCriteriaValue.getPropertyName().trim().toUpperCase();
+        Object value = literalCriteriaValue.getValue();
+        String operator = comparisonCriteria.operator().toString();
+        
+        switch(field)
+        {
+        case "ID":
+        {
+        	Integer idValue = Integer.valueOf(value.toString());
+
+        	Path<Integer> provinciaIdPath = Expressions.path(Integer.class, qProvincia, "id");
+        	Constant<Integer> constant = (Constant<Integer>) Expressions.constant(idValue);
+
+        	return Expressions.predicate(queryDslOpsConverter.convertToAnotherAttribute(operator), provinciaIdPath, constant);
+        }
+        case "NOMBRE":
+        {
+        	String descripcionValue = value.toString();
+        	
+        	Path<String> provinciaNombrePath = Expressions.path(String.class, qProvincia, "nombre");
+        	Constant<String> constant = (Constant<String>) Expressions.constant(descripcionValue);
+        	
+        	return Expressions.predicate(queryDslOpsConverter.convertToAnotherAttribute(operator), provinciaNombrePath, constant);
+        }
+        case "PAISID":
+        case "PAIS.ID":
+        {
+        	Integer paisIdValue = Integer.valueOf(value.toString());
+
+        	Path<Integer> paisIdPath = Expressions.path(Integer.class, qProvincia.pais, "id");
+        	Constant<Integer> constant = (Constant<Integer>) Expressions.constant(paisIdValue);
+
+        	return Expressions.predicate(queryDslOpsConverter.convertToAnotherAttribute(operator), paisIdPath, constant);
+        }
+        case "PAIS.NOMBRE":
+        {
+        	String paisNombreValue = value.toString();
+
+        	Path<String> paisNombrePath = Expressions.path(String.class, qProvincia.pais, "nombre");
+        	Constant<String> constant = (Constant<String>) Expressions.constant(paisNombreValue);
+        	
+        	return Expressions.predicate(queryDslOpsConverter.convertToAnotherAttribute(operator), paisNombrePath, constant);
+        }
+        case "PAIS.PREFIJO":
+        {
+        	Integer paisPrefijoValue = Integer.valueOf(value.toString());
+
+        	Path<Integer> paisPrefijoPath = Expressions.path(Integer.class, qProvincia.pais, "prefijo");
+        	Constant<Integer> constant = (Constant<Integer>) Expressions.constant(paisPrefijoValue);
+        	
+        	return Expressions.predicate(queryDslOpsConverter.convertToAnotherAttribute(operator), paisPrefijoPath, constant);
+        }
+        }
+        
+        return null;
     }
 
-    private void execComparisonCriteria(ComparisonCriteria comparisonCriteria) {
+    private PropertyCriteriaValue getPropertyCriteriaValue(ComparisonCriteria comparisonCriteria) {
+    	
+    	if(comparisonCriteria.getLeft() instanceof PropertyCriteriaValue) {
+    		return (PropertyCriteriaValue) comparisonCriteria.getLeft();
+    	}
+    	
+    	if(comparisonCriteria.getRight() instanceof PropertyCriteriaValue) {
+    		return (PropertyCriteriaValue) comparisonCriteria.getRight();
+    	}
+    	
+    	return null;
+    }
 
-        if(comparisonCriteria.getLeft() instanceof PropertyCriteriaValue && comparisonCriteria.getRight() instanceof LiteralCriteriaValue) {
-
-            PropertyCriteriaValue propertyCriteriaValue = (PropertyCriteriaValue) comparisonCriteria.getLeft();
-            LiteralCriteriaValue literalCriteriaValue = (LiteralCriteriaValue) comparisonCriteria.getRight();
-            
-            String field = propertyCriteriaValue.getPropertyName().trim().toUpperCase();
-            Object value = literalCriteriaValue.getValue();
-            
-            switch(field)
-            {
-            case "ID":
-            {
-            	Integer idValue = (Integer) value;
-            	BooleanExpression exp = qProvincia.id.eq(idValue);
-            	this.expression = this.expression == null ? exp : this.expression.and(exp);
-            	break;
-            }
-            case "NOMBRE":
-            {
-            	String descripcionValue = (String) value;
-            	BooleanExpression exp = qProvincia.nombre.eq(descripcionValue);
-                this.expression = this.expression == null ? exp : this.expression.and(exp);
-            	break;
-            }
-            case "PAISID":
-            case "PAIS.ID":
-            {
-            	Integer paisIdValue = Integer.valueOf(value.toString());
-            	BooleanExpression exp = qProvincia.pais.id.eq(paisIdValue);
-                this.expression = this.expression == null ? exp : this.expression.and(exp);
-            	break;
-            }
-            case "PAIS.NOMBRE":
-            {
-            	String paisNombreValue = (String) value;
-            	BooleanExpression exp = qProvincia.pais.nombre.eq(paisNombreValue);
-                this.expression = this.expression == null ? exp : this.expression.and(exp);
-            	break;
-            }
-            case "PAIS.PREFIJO":
-            {
-            	Integer paisPrefijoValue = Integer.valueOf(value.toString());
-            	BooleanExpression exp = qProvincia.pais.prefijo.eq(paisPrefijoValue);
-                this.expression = this.expression == null ? exp : this.expression.and(exp);
-            	break;
-            }
-            }
-        }
+    private LiteralCriteriaValue getLiteralCriteriaValue(ComparisonCriteria comparisonCriteria) {
+    	
+    	if(comparisonCriteria.getLeft() instanceof LiteralCriteriaValue) {
+    		return (LiteralCriteriaValue) comparisonCriteria.getLeft();
+    	}
+    	
+    	if(comparisonCriteria.getRight() instanceof LiteralCriteriaValue) {
+    		return (LiteralCriteriaValue) comparisonCriteria.getRight();
+    	}
+    	
+    	return null;
     }
 
     private void buildFromLimit(LimitOperation operation) throws ODataException {
